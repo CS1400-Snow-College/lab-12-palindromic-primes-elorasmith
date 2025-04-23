a2ba4ac3c20ad79eae87e355e48c0b8a097cd836
elorasmith <eloraasmith@gmail.com>
2025-04-23 10:01:12 -0600

    IsPrime method

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Elora Smith, 4/23/25, Lab 12 Palindromic Primes
+
+using System.Diagnostics;
+
+Debug.Assert(IsPrime(7) == true);
+Debug.Assert(IsPrime(10) == false);
+Debug.Assert(IsPrime(1) == false);
+Debug.Assert(IsPrime(104729) == true);
+Debug.Assert(IsPrime(25) == false);
+
+static bool IsPrime(int num)
+{
+    if (num <= 1)
+        return false;
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+
